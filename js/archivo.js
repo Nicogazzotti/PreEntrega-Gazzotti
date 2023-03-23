096995e9b6686bfa44b05d2cbacd47e48d7ec88a
Nicogazzotti <nicogazzotti@gmail.com>
2023-03-23 19:02:13 -0300

    Commit 2

--- js/archivo.js
+++ js/archivo.js
@@ -1,38 +1,48 @@
-let tipoPizza= prompt('Ingrese "1" si quiere pizza chica o "2" si quiere pizza grande')
 
-while ((tipoPizza != 1 ) && (tipoPizza != 2)){
-    tipoPizza=prompt('No es valido, Ingrese "1" si quiere pizza pequeña o "2" si quiere pizza grande ')
-}
-let tamañoPizza=""
-if (tipoPizza==1){
-    tamañoPizza="chica"
-}else{
-    tamañoPizza="grande"
-}
-console.log("Usted va a ordenar el tipo de pizza "+tamañoPizza)
-let precio=Precio(tamañoPizza)
+let cantidad=Cantidadpizzas()
+const pizzas=[]
+let sumaprecios=0
+for (let i=1; i<cantidad+1 && i<5+1; i++){
+    let tamañoPizza=TipoPizza(i)
+    let precio=Precio(tamañoPizza)
+    if (i==1){
+        console.log("El precio individual de la pizza "+tamañoPizza+" es de "+ precio)
+    }
+    
+    let gustoPizza= prompt("Ingrese que gusto quiere para la pizza " + i +" (Solo disponible hasta 5 pizzas o las que se pidan ). Los gustos disponibles son napolitana, muzzarella, fugazzeta, cuatro quesos y calabresa.")
+    
+    pizzas.push({nropizza:i, gusto:gustoPizza, tamaño:tamañoPizza, precio: precio })
+    sumaprecios+=precio
+    if (cantidad==i){
+        Imprimir(cantidad,sumaprecios,pizzas)
+    }
 
-console.log("El precio individual de la pizza "+tamañoPizza+" es de "+ precio)
 
-let cantidad=parseInt(prompt('Ingrese la cantidad de pizzas que va a pedir'))
-while(cantidad==0 || cantidad==null){
-    cantidad=parseInt(prompt('El valor ingresado no es valido, Ingrese nuevamente la cantidad de pizzas que va a pedir'))
-}
-if (cantidad==1){
-    console.log("Usted va a pedir "+cantidad+" pizza")
-}
-else{
-    console.log("Usted va a pedir "+cantidad+" pizzas")
 }
 
-
-for (let i=1; i<cantidad+1 && i<4+1; i++){
-    let gustoPizza= prompt("Ingrese que gusto quiere para la pizza " + i +" (Solo disponible hasta las primeras 4 pizzas o las que se pidan ). Los gustos disponibles son napolitana, muzzarella, fugazzeta, cuatro quesos y calabresa.")
-
-    console.log('La pizza ' + i + ' es de '+gustoPizza)
+function Cantidadpizzas(){
+    let cantidad=parseInt(prompt('Ingrese la cantidad de pizzas que va a pedir'))
+    while(cantidad===0 || cantidad ===null || isNaN(cantidad) ){
+        cantidad=parseInt(prompt('El valor ingresado no es valido, Ingrese nuevamente la cantidad de pizzas que va a pedir'))
+    }
+    
+    return cantidad
 }
 
-Total(precio,cantidad)
+function TipoPizza(i){
+    let tipoP=prompt('Para la piiza ' + i+ ': Ingrese "1" si quiere pizza chica o "2" si quiere pizza grande')
+
+    while ((tipoP != 1 ) && (tipoP != 2)){
+        tipoP=prompt('No es valido, Ingrese "1" si quiere pizza chica o "2" si quiere pizza grande ')
+    }
+    let tamañoP=""
+    if (tipoP==1){
+        tamañoP="chica"
+    }else{
+        tamañoP="grande"
+    }
+    return tamañoP
+}
 
 function Precio(tamP){
     let precio=0
@@ -45,16 +55,17 @@ function Precio(tamP){
     }
     return precio
 }
-function Total(p,can){
-    let total=p*can
-    Imprimir(can,total)
-}
-function Imprimir(can,tot){
+
+function Imprimir(can,tot,pizzas){
+    pizzas.forEach((pizza)=>{console.log("Para la pizza "+ pizza.nropizza+" usted va a ordenar una "+pizza.gusto+" "+ pizza.tamaño+ " y vale "+pizza.precio)})
+    
+
     if (can==1){
         console.log("Su pedido es de " +can+" pizza y vale "+tot)
     }
     else{
         console.log("Su pedido es de " +can+" pizzas y vale "+tot)
     }
+    // console.log(pizzas.join(","))
     
 }
